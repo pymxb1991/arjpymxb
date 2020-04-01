176a53661665eb7caa5cabc01ab1ac61a071e846
pymxb1991 <pymxb1991@163.com>
2020-04-02 06:49:12 +0800

    初始最简单转账测试

--- spring-easy-account01/src/test/java/com/pymxb/test/AccountServiceTest.java
+++ spring-easy-account01/src/test/java/com/pymxb/test/AccountServiceTest.java
@@ -1,14 +1,11 @@
 package com.pymxb.test;
 
-import com.pymxb.domain.Account;
 import com.pymxb.service.IAccountService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.List;
 
 /**
  * 使用Junit单元测试：测试我们的配置
@@ -19,48 +16,10 @@ public class AccountServiceTest {
 
     @Autowired
     private IAccountService as;
-    @Test
-    public void testFindAll() {
-
-        //3.执行方法
-        List<Account> accounts = as.findAllAccount();
-        for(Account account : accounts){
-            System.out.println(account);
-        }
-    }
 
     @Test
-    public void testFindbyId() {
-
-        //3.执行方法
-        Account account = as.findAccountById(1);
-        System.out.println(account);
+    public  void testTransfer(){
+        as.transfer("aaa","bbb",100f);
     }
 
-    @Test
-    public void testSave() {
-
-        Account account = new Account();
-        account.setName("test");
-        account.setMoney(12345f);
-        //3.执行方法
-        as.saveAccount(account);
-
-    }
-
-    @Test
-    public void testUpdate() {
-
-        //3.执行方法
-        Account account = as.findAccountById(4);
-        account.setMoney(23456f);
-        as.updateAccount(account);
-    }
-
-    @Test
-    public void testDelete() {
-
-        //3.执行方法
-        as.deleteAccount(4);
-    }
 }
