70ad8b4488fa2ba3ea13309dd7b4aefb5ce2e767
pymxb1991 <pymxb1991@163.com>
2020-04-06 06:40:45 +0800

    spring AOP  注解的方式配置AOP      1、spring 使用注解配置AOP 时，直接用环绕通知      2、配置spring开启注解AOP的支持

---
+++ spring-aop03-anno/src/main/java/com/pymxb/service/impl/AccountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.pymxb.service.impl;
+
+
+import com.pymxb.service.IAccountService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 账户的业务层实现类
+ */
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService {
+
+    @Override
+    public void saveAccount() {
+        System.out.println("执行了保存");
+   //     ;int  num = 1/0;
+    }
+
+    @Override
+    public void updateAccount(int i) {
+        System.out.println("执行了更新"+i);
+
+    }
+
+    @Override
+    public int deleteAccount() {
+        System.out.println("执行了删除");
+        return 0;
+    }
+}
