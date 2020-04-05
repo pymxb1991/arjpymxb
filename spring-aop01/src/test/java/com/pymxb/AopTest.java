873b393c798331bce8d9fef5a59f8e1d218ce199
pymxb1991 <pymxb1991@163.com>
2020-04-05 22:31:09 +0800

    spring AOP execution 表达式使用学习
    8、实际开发中切入点表达式的通常写法：
                        切到业务层实现类下的所有方法
                            * com.itheima.service.impl.*.*(..)

--- spring-aop01/src/test/java/com/pymxb/AopTest.java
+++ spring-aop01/src/test/java/com/pymxb/AopTest.java
@@ -13,5 +13,10 @@ public class AopTest {
         IAccountService as =  (IAccountService) ac.getBean("accountService");
         //3、执行方法
         as.saveAccount();
+        // 由于切入点配置了增强saveAccount 方法，所以在执行saveAccount方法之前，进行了日志记录；
+        //如果执行其它方法如何进行操作,查看配置文件笔记 execution  各种写法
+        as.updateAccount(1);
+        as.deleteAccount();
+
     }
 }
